396a604e84f72730dc3a1f90c87e6fa4be5da464
caroline gorham <caroling@carolines-MacBook-Pro.local>
2021-09-07 20:13:05 -0400

    apps

---
+++ app.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+APIKEY= 'RN6CGXIAQ7RMQG9G'
+ticker='AAPL'
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={}&apikey={}'.format(ticker,APIKEY)
+
+r = requests.get(url)
+data = r.json()
